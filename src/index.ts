79692725316150d56e790f5e44be35421bf5fc63
RumbleFrog <xxlosttechxx@gmail.com>
2018-07-06 23:59:01 -0400

    Working commit :>

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+interface ThrottleOption {
+    container: string,
+    id: string,
+    duration?: number,
+    usages?: number
+}
+
+interface Throttler {
+    start: number,
+    usages: number,
+    timeout: NodeJS.Timer,
+}
+
+export class Throttle {
+
+    private _whitelist: Array<string>;
+    private _throttles: Map<string, Map<string, Throttler>>;
+
+    constructor(whitelist?: Array<string>) {
+        this._whitelist = whitelist || [];
+        this._throttles = new Map();
+    }
+
+    public throttle(options: ThrottleOption) {
+        if (this._whitelist.includes(options.id)) return false;
+
+        let container = this._throttles.get(options.container);
+
+        if (!container) {
+            container = new Map();
+            this._throttles.set(options.container, container);
+        }
+
+        let throttle = container.get(options.id);
+
+        if (!throttle) {
+            throttle = {
+                start: Date.now(),
+                usages: 1,
+                timeout: setTimeout(() => {
+                    container.delete(options.id)
+                }, options.duration * 1000)
+            }
+
+            container.set(options.id, throttle);
+
+            return true;
+        }
+
+        if (throttle.usages + 1 > options.usages) {
+            return false;
+        }
+
+        throttle.usages++;
+
+        container.set(options.id, throttle);
+
+        return true;
+    }
+
+    public getThrottler(options: ThrottleOption) {
+        return this._throttles.get(options.container).get(options.id);
+    }
+}
---
+++ tests/throttled.js
@@ -0,0 +1,37 @@
+const { Throttle } = require('../dist');
+
+function r() {
+    let t = new Throttle();
+
+    console.log(t.throttle({
+        container: '1',
+        id: 'fishy',
+        usages: 2,
+        duration: 5
+    }));
+
+    console.log(t.throttle({
+        container: '1',
+        id: 'fishy',
+        usages: 2,
+        duration: 5
+    }));
+
+    console.log(t.throttle({
+        container: '1',
+        id: 'fishy',
+        usages: 2,
+        duration: 5
+    }));
+
+    setTimeout(() => {
+        console.log(t.throttle({
+            container: '1',
+            id: 'fishy',
+            usages: 2,
+            duration: 10
+        }));
+    }, 1000 * 6)
+}
+
+r();
